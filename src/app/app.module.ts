6cdf86f87f678fc3735272ac1e9b09d5906a09c1
Alberto Alfieri <alberto.alfieri@live.it>
2020-12-21 18:47:21 +0100

    added about section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { pjSearchFlightComponent } from './components/search-flight/search-flight.component';
 import { PjToolbarComponent } from './components/toolbar/toolbar.component';
+import { PjAboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     pjSearchFlightComponent,
     PjToolbarComponent,
+    PjAboutComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'pj-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
+})
+export class PjAboutComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
