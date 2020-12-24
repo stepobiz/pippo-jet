ca3f89c8d14c0c7b46c3b3a8add7ef1ae0464c3e
Alberto Alfieri <alberto.alfieri@live.it>
2020-12-24 01:52:26 +0100

    search-flight complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list';
 
@@ -20,7 +22,7 @@ import { PjAboutComponent } from './components/about/about.component';
 import { PjHeaderComponent } from './components/header/header.component';
 import { PjHomePageComponent } from './components/home-page/home-page.component';
 import { PjFlightPageComponent } from './components/flight-page/flight-page.component';
-import { MatNativeDateModule } from '@angular/material/core';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 import { PjSliderComponent } from './components/slider/slider.component';
 import { PjTopMenuComponent } from './components/topmenu/topmenu.component';
 import { PgFooterComponent } from './components/pgfooter/pgfooter.component';
@@ -53,15 +55,16 @@ import { PgFooterComponent } from './components/pgfooter/pgfooter.component';
     MatCardModule,
     MatSelectModule,
     MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatNativeDateModule,
     MatMenuModule,
-
 
     NgImageSliderModule,
 
     
   ],
-  providers: [],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'it-IT' }],
   bootstrap: [AppComponent]
 })
 
