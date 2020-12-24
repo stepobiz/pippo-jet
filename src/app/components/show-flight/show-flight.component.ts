a971aa3df0de6d1fcd4aabf173e0b718ecac6610
giacomo <trinchillogiacomomattia@gmail.com>
2020-12-24 17:01:20 +0100

    modified show-fligth.component

--- src/app/components/show-flight/show-flight.component.ts
+++ src/app/components/show-flight/show-flight.component.ts
@@ -10,15 +10,15 @@ export class PjShowFlightComponent implements OnInit {
 
     ngOnInit(): void { }
 
-    flight = [
-        {numerovolo: 'CC54685', partenza: 'Napoli', destinazione: 'Roma'},
-        {numerovolo: 'CD54865', partenza: 'Roma', destinazione: 'Roma'},
-        {numerovolo: 'CD54865', partenza: 'Roma', destinazione: 'Roma'},
-        {numerovolo: 'DS45856', partenza: 'Milano', destinazione: 'Roma'},
-        {numerovolo: 'RF56895', partenza: 'Roma', destinazione: 'Casoria'},
-        {numerovolo: 'CB58964', partenza: 'Torino', destinazione: 'Milano'},
-        {numerovolo: 'GH55689', partenza: 'Genova', destinazione: 'Roma'},
-        {numerovolo: 'JU54866', partenza: 'Capri', destinazione: 'Roma'}
+    flights = [
+        {numerovolo: 'CC54685', partenza: 'Napoli', destinazione: 'Torino', ritardo: '15 min', prezzo: '30€'},
+        {numerovolo: 'CD54865', partenza: 'Roma', destinazione: 'Torino', ritardo: '10 min', prezzo: '20€'},
+        {numerovolo: 'CD54865', partenza: 'Torino', destinazione: 'Roma', ritardo: ' ', prezzo: '12€'},
+        {numerovolo: 'DS45856', partenza: 'Napoli', destinazione: 'Roma', ritardo: '12 min', prezzo: '15€'},
+        {numerovolo: 'RF56895', partenza: 'Roma', destinazione: 'Napoli', ritardo: '7 min', prezzo: '10€'},
+        {numerovolo: 'CB58964', partenza: 'Torino', destinazione: 'Napoli', ritardo: '8 min', prezzo: '10€'},
+        {numerovolo: 'GH55689', partenza: 'Genova', destinazione: 'Roma', ritardo: '5 min', prezzo: '25€'},
+        {numerovolo: 'JU54866', partenza: 'Genova', destinazione: 'Torino', ritardo: '2 min', prezzo: '20€'}
       ];
 }
 
