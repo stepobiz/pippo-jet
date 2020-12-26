61f549d99133af00ea4d7978ec0e8c4edc2ec828
Marika <mr.marikaraimo@gmail.com>
2020-12-26 23:12:05 +0100

    add nations to login.component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,6 +12,22 @@ export class PjLoginComponent implements OnInit {
     emailFormControl = new FormControl('', [
         Validators.required,
         Validators.email,
-      ]);
+    ]);
+
+    nations = [
+        {value: 'Australia', viewValue: 'Australia'},
+        {value: 'Belgium', viewValue: 'Belgio'},
+        {value: 'Botswana', viewValue: 'Botswana'},
+        {value: 'Canada', viewValue: 'Canada'},
+        {value: 'China', viewValue: 'Cina'},
+        {value: 'Colombia', viewValue: 'Colombia'},
+        {value: 'France', viewValue: 'Francia'},
+        {value: 'Germany', viewValue: 'Germania'},
+        {value: 'Italy', viewValue: 'Italia'},
+        {value: 'Spain', viewValue: 'Spagna'},
+        {value: 'Southafrica', viewValue: 'Sudafrica'},
+        {value: 'USA', viewValue: 'Torino'}         
+    ];
+
     ngOnInit(): void { }
 }
