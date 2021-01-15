d250e4e991d15f245f22d56e926d875fb36cf730
Alberto Alfieri <alberto.alfieri@live.it>
2021-01-15 14:26:02 +0100

    alberto module

---
+++ src/app/modules/pj-alberto/components/aacomponente2/aacomponente2.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'aacomponente2',
+    templateUrl: './aacomponente2.component.html',
+    styleUrls: ['./aacomponente2.component.scss']
+})
+export class AAComponente2Component implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
